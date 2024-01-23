fd82acf4143707193ea25580fbb309ac07709acb
JeremPlt-Kraken <peltierj57@gmail.com>
2024-01-23 17:44:38 +0100

    Update and improve script + Add track

--- csp_parser.py
+++ csp_parser.py
@@ -39,6 +39,8 @@ def read_ini_file(file_path):
 
     return ini_data
 
+
+
 def read_json_file(file_path):
     """
     Reads a JSON file and returns its content.
@@ -51,6 +53,8 @@ def read_json_file(file_path):
     """
     with open(file_path, 'r') as file:
         return json.load(file)
+    
+    
 
 def process_data(ini_data, json_template):
     """
@@ -72,6 +76,8 @@ def process_data(ini_data, json_template):
             result[category] = cat_result
     print("Processing completed")
     return result
+
+
 
 def process_category(ini_data, category_details):
     """
@@ -109,52 +115,132 @@ def process_category(ini_data, category_details):
 
     return {k: v for k, v in category_result.items() if v}
 
+
+
 def process_sub_category(ini_data, sub_details, sub_category, category_result):
     """
     Processes a sub-category of data based on the given details.
-
-    Args:
-    ini_data (dict): Dictionary containing INI file data.
-    sub_details (dict): Details of the sub-category to be processed.
-    sub_category (str): Name of the sub-category.
-    category_result (dict): Dictionary to store the results of processing.
     """
     tag = sub_details.get("tag", "").strip("[]")
     tags = sub_details.get("tags", [])
     if not tags and tag:
         tags = [tag]
 
-    print(f"    Tags for {sub_category}: {tags}")
+    foreach = sub_details.get("foreach", False)
+    only_one = sub_details.get("only_one", False)
 
+    if foreach:
+        process_foreach_sub_category(ini_data, sub_details, sub_category, category_result, tags, only_one)
+    else:
+        process_single_sub_category(ini_data, sub_details, sub_category, category_result, tags)
+        
+        
+
+def process_foreach_sub_category(ini_data, sub_details, sub_category, category_result, tags, only_one):
+    """
+    Processes a sub-category of data with 'foreach' logic.
+    """
+    found = False
+    sub_category_result = {}
+
+    if tags[0].endswith("..."):
+        # Pour les tags avec '...'
+        exact_tag = tags[0].strip("[].")
+        index = 0
+        for ini_tag in ini_data.keys():
+            if ini_tag == exact_tag:
+                if process_conditions(ini_data, sub_details, ini_tag, sub_category_result, index, only_one):
+                    found = True
+                    if only_one:
+                        break
+                index += 1
+    else:
+        # Pour les tags normaux avec indexation
+        index = 0
+        not_found_count = 0
+        while not_found_count < 2:
+            foreach_tag = tags[0].replace("_N", f"_{index}")
+            if foreach_tag in ini_data:
+                if process_conditions(ini_data, sub_details, foreach_tag, sub_category_result, index, only_one):
+                    found = True
+                    if only_one:
+                        category_result[sub_category] = True
+                        return  # Sortie immédiate si une condition est remplie et only_one est vrai
+                not_found_count = 0
+            else:
+                not_found_count += 1
+            index += 1
+
+    if not only_one:
+        category_result[sub_category] = sub_category_result
+    elif not found:
+        category_result[sub_category] = False
+
+
+
+def process_single_sub_category(ini_data, sub_details, sub_category, category_result, tags):
+    """
+    Processes a single sub-category of data.
+    """
+    entry = sub_details.get("entry")
+    entries = sub_details.get("entries", {})
+    return_value = sub_details.get("return_value", False)
+    
+    # Vérifie si les conditions dans 'entries' sont remplies
+    for tag in tags:
+        tag = tag.strip("[]")
+        if tag in ini_data:
+            conditions_met = True
+            if entries:
+                for k, v in entries.items():
+                    possible_values = v.split('|')
+                    ini_value = ini_data[tag].get(k, None)
+                    if not any(val.strip() == ini_value for val in possible_values):
+                        conditions_met = False
+                        break
+            
+            if conditions_met:
+                if return_value and entry:
+                    if isinstance(entry, list):
+                        entry_values = {e.lower(): ini_data[tag].get(e, "") for e in entry}
+                        category_result[sub_category] = entry_values
+                    else:
+                        entry_value = ini_data[tag].get(entry, "")
+                        category_result[sub_category] = entry_value
+                else:
+                    category_result[sub_category] = True
+                return  # Sort de la boucle dès qu'une condition est remplie
+
+
+
+
+def process_conditions(ini_data, sub_details, tag, sub_category_result, index, only_one):
+    """
+    Processes conditions for a sub-category and updates the result.
+    """
     entry = sub_details.get("entry")
     entries = sub_details.get("entries", {})
     return_value = sub_details.get("return_value", False)
 
-    if not entries and not entry:
-        # New logic to handle the absence of "entry" or "entries"
-        if any(tag.strip("[]") in ini_data for tag in tags):
-            print(f"    Tag found for {sub_category}, no specific entry required")
-            category_result[sub_category] = True
-        else:
-            print(f"    Tag not found for {sub_category}")
-        return
+    conditions_met = True
+    if entries:
+        for k, v in entries.items():
+            possible_values = v.split('|')
+            ini_value = ini_data[tag].get(k, None)
+            if not any(val.strip() == ini_value for val in possible_values):
+                conditions_met = False
+                break
 
-    if any(tag.strip("[]") in ini_data for tag in tags):
-        print(f"    Tag found for {sub_category}")
-        if check_entries(ini_data, tags, entries):
-            if return_value and entry:
-                for tag in tags:
-                    tag = tag.strip("[]")
-                    if tag in ini_data and entry in ini_data[tag]:
-                        category_result[sub_category] = ini_data[tag].get(entry)
-                        print(f"      Value returned for {sub_category}: {category_result[sub_category]}")
-                        break
-            else:
-                category_result[sub_category] = True
+    if conditions_met:
+        if return_value and entry:
+            series_result = {e.lower(): ini_data[tag].get(e, "") for e in entry} if isinstance(entry, list) else ini_data[tag].get(entry, "")
+            sub_category_result[str(index)] = series_result
         else:
-            print(f"    Entries do not match for {sub_category}")
-    else:
-        print(f"    Tag not found for {sub_category}")
+            sub_category_result[str(index)] = True
+        return True
+    return False
+
+
 
 def check_entries(ini_data, tags, entries=None):
     """
@@ -196,6 +282,13 @@ def check_entries(ini_data, tags, entries=None):
                     print(f"Entry '{key}' does not contain '{search_value}' for tag '{tag}'.")
                     all_entries_match = False
                     break
+            elif '|' in value:
+                # Split the value by '|' and check if any of the values is present
+                possible_values = value.split('|')
+                if key not in data or not any(val.strip() in data[key] for val in possible_values):
+                    print(f"None of the possible values for '{key}' match for tag '{tag}'.")
+                    all_entries_match = False
+                    break
             else:
                 # Exact match for other values
                 if key not in data or data[key] != value:
@@ -210,12 +303,14 @@ def check_entries(ini_data, tags, entries=None):
     print("No match for the specified tags and entries.")
     return False
 
+
+
 # Paths for Cars and Tracks
-ini_file_path = './cars/ext_config.ini'
-json_file_path = './cars/car_csp.json'
+# ini_file_path = './cars/ext_config.ini'
+# json_file_path = './cars/car_csp.json'
 # For Tracks
-# ini_file_path = './tracks/ext_config.ini'
-# json_file_path = './tracks/track_csp.json'
+ini_file_path = './tracks/ext_config.ini'
+json_file_path = './tracks/track_csp.json'
 
 # Read and process the data
 ini_data = read_ini_file(ini_file_path)
